445d2018e333479b7a7b93431112dbcb528fdf19
ladismrkolj <ladismrkolj@gmail.com>
2020-09-20 14:18:18 +0200

    Added auto chamber fan, chamber vent algorithm in temperature.cpp and chamber vent servo (1)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2398,15 +2398,15 @@
  * Set this manually if there are extra servos needing manual control.
  * Set to 0 to turn off servo support.
  */
-//#define NUM_SERVOS 2 // Servo index starts with 0 for M280 command
+#define NUM_SERVOS 2 // Servo index starts with 0 for M280 command
 
 // (ms) Delay  before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
 // If the servo can't reach the requested position, increase it.
-#define SERVO_DELAY { 300 }
+#define SERVO_DELAY { 300, 300 }
 
 // Only power servos during movement, otherwise leave off to prevent jitter
 //#define DEACTIVATE_SERVOS_AFTER_MOVE
 
-// Edit servo angles with M281 and save to EEPROM with M500
-//#define EDITABLE_SERVO_ANGLES
+// Allow servo angle to be edited and saved to EEPROM
+#define EDITABLE_SERVO_ANGLES
